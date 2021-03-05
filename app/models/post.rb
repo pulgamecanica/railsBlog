7ba21f53e6c2c912f8e408ae2f1e932aa4c57e28
pulgamecanica11 <pulgamecanica11@gmail.com>
2021-03-05 18:27:11 +0000

    Added the delete icon and the Pragraph and Image icons with font awesome

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ApplicationRecord
   has_many :elements
 
   has_one_attached :header_image
+  before_update :set_published_time
+
+  def set_published_time
+  	if self.published
+  		self.published_at = Time.now
+  	else
+  		self.published_at = nil
+  	end
+
+  end
 end
