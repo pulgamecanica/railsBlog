7a69dd65001a72e7675a18b8e75d218d59a5489c
pulgamecanica11 <pulgamecanica11@gmail.com>
2021-04-26 18:35:29 +0100

    Added update.js wich allows saving without reloading

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,18 @@ class Post < ApplicationRecord
   has_many :elements
 
   has_one_attached :header_image
-  before_update :set_published_time
 
-  def set_published_time
-  	if self.published
-  		self.published_at = Time.now
-  	else
-  		self.published_at = nil
-  	end
+  validates_presence_of :title, :description
+  validates_length_of :description, within: 50..300
+  
+  #UPDATE PUBLISH TIME WITH CALLBACK
+  # before_update :set_published_time
+  # def set_published_time
+  # 	if self.published
+  # 		self.published_at = Time.now
+  # 	else
+  # 		self.published_at = nil
+  # 	end
 
-  end
+  # end
 end
